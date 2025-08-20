33ef3ddafb46dbdac897cc64aea55d77d6634d1a
shubham9049 <shubham9049>
2025-08-20 16:09:23 +0530

    Add changes

--- components/AddBlogs.tsx
+++ components/AddBlogs.tsx
@@ -12,6 +12,7 @@ interface BlogPost {
   author: string;
   tags?: string;
   coverImage?: string;
+  schemaMarkup?: string[]; // 👈 add this
 }
 
 const AddBlog = ({
@@ -49,8 +50,8 @@ const AddBlog = ({
         coverImage: null,
 
         schemaMarkup:
-          (existingBlog as any).schemaMarkup?.length > 0
-            ? (existingBlog as any).schemaMarkup
+          existingBlog.schemaMarkup && existingBlog.schemaMarkup.length > 0
+            ? existingBlog.schemaMarkup
             : [""],
       });
     }
