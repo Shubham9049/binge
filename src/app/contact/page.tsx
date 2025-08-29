0e61f924a01ae4f652b48de2116e9c8ed8374c04
shubham9049 <shubham9049>
2025-08-29 17:30:58 +0530

    Add chnages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -85,7 +85,7 @@ export default function ContactPage() {
         <div className="relative z-10 text-center text-white px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-4">Contact Us</h1>
           <p className="text-lg md:text-2xl max-w-2xl mx-auto">
-            Let's not waste time & get started. Shall we?
+            Let&#39;s not waste time & get started. Shall we?
           </p>
           <button
             onClick={scrollToNext}
